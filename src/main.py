4925dc8b5dcf1a52f09ca066d0f4e8fa96526635
CongPPT <congppt2002@gmail.com>
2024-11-19 20:22:18 +0700

    update app

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from src.db.database import DATABASE_MANAGER
 from user.router import user_router
 
 
-async def lifespan(app: FastAPI):
+async def lifespan(_app: FastAPI):
     yield
     # close cache connections
     await CACHE.aclose()
@@ -17,19 +17,19 @@ async def lifespan(app: FastAPI):
     await DATABASE_MANAGER.aclose_connections()
 
 
-_app = FastAPI()
+app = FastAPI()
 
 
 for middleware in middlewares:
     if isinstance(middleware, tuple):
-        _app.add_middleware(middleware[0], **middleware[1])
+        app.add_middleware(middleware[0], **middleware[1])
     else:
-        _app.add_middleware(middleware)
+        app.add_middleware(middleware)
 
 routers: set = {auth_router, user_router, config_router}
 for router in routers:
-    _app.include_router(router, prefix=f"/{API_PREFIX}")
+    app.include_router(router, prefix=f"/{API_PREFIX}")
 
-@_app.get("/health-check")
+@app.get("/health-check")
 async def health_check():
     return "App is running"
